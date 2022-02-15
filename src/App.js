54097465f3df90008fd45affa8348328c1a30550
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-02-15 22:13:09 +0700

    adding file & folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+import './App.css'
+
+import { Navbar, HomePage, Cryptocurrencies, News, Exchanges, CryptoDetails } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrency">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:id">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      {/* <div className="footer">
+
+      </div> */}
+    </div>
   )
 }
 
